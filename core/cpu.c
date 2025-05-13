4c840a791b0ad27bcc8ca631a74c5c1ff746988c
dgz <dgz@dgz.me>
2025-05-13 18:41:10 -0400

    Implement LD A, (FF00+u8) ($F0) instruction
    Title.
    
    Signed-off-by: dgz <dgz@dgz.me>

--- core/cpu.c
+++ core/cpu.c
@@ -185,6 +185,7 @@ void agoge_core_cpu_run(struct agoge_core_cpu *const cpu,
 		[CPU_OP_POP_HL] = &&pop_hl,
 		[CPU_OP_PUSH_HL] = &&push_hl,
 		[CPU_OP_LD_MEM_U16_A] = &&ld_mem_u16_a,
+		[CPU_OP_LD_A_MEM_FF00_U8] = &&ld_a_mem_ff00_u8,
 		[CPU_OP_POP_AF] = &&pop_af,
 		[CPU_OP_DI] = &&di,
 		[CPU_OP_PUSH_AF] = &&push_af
@@ -312,6 +313,10 @@ push_hl:
 
 ld_mem_u16_a:
 	agoge_core_bus_write(cpu->bus, read_u16(cpu), cpu->reg.a);
+	DISPATCH();
+
+ld_a_mem_ff00_u8:
+	cpu->reg.a = agoge_core_bus_read(cpu->bus, 0xFF00 + read_u8(cpu));
 	DISPATCH();
 
 pop_af:
