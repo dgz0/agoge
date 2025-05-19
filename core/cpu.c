c37d789df7957bf95a748483f88cd4416924ab91
dgz <dgz@dgz.me>
2025-05-19 19:55:41 -0400

    Implement RET C ($D8) instruction
    Title.
    
    Signed-off-by: dgz <dgz@dgz.me>

--- core/cpu.c
+++ core/cpu.c
@@ -346,6 +346,7 @@ void agoge_core_cpu_run(struct agoge_core_cpu *const cpu,
 		[CPU_OP_POP_DE] = &&pop_de,
 		[CPU_OP_PUSH_DE] = &&push_de,
 		[CPU_OP_SUB_A_U8] = &&sub_a_u8,
+		[CPU_OP_RET_C] = &&ret_c,
 		[CPU_OP_LD_MEM_FF00_U8_A] = &&ld_mem_ff00_u8_a,
 		[CPU_OP_POP_HL] = &&pop_hl,
 		[CPU_OP_PUSH_HL] = &&push_hl,
@@ -700,6 +701,10 @@ push_de:
 
 sub_a_u8:
 	alu_sub(cpu, read_u8(cpu));
+	DISPATCH();
+
+ret_c:
+	ret_if(cpu, cpu->reg.f & CPU_FLAG_CARRY);
 	DISPATCH();
 
 ld_mem_ff00_u8_a:
--- core/disasm.c
+++ core/disasm.c
@@ -114,6 +114,7 @@ static const struct disasm_entry op_tbl[] = {
 	[CPU_OP_POP_DE] = { .op = OP_NONE, .fmt = "POP DE" },
 	[CPU_OP_PUSH_DE] = { .op = OP_NONE, .fmt = "PUSH DE" },
 	[CPU_OP_SUB_A_U8] = { .op = OP_U8, .fmt = "SUB A, $%02X" },
+	[CPU_OP_RET_C] = { .op = OP_NONE, .fmt = "RET C" },
 	[CPU_OP_LD_MEM_FF00_U8_A] = { .op = OP_U8,
 				      .fmt = "LD (FF00+$%02X), A" },
 	[CPU_OP_POP_HL] = { .op = OP_NONE, .fmt = "POP HL" },
