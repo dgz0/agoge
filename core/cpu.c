e306aac743a120a6720cfbc867b2e521c90628f5
dgz <dgz@dgz.me>
2025-05-26 17:20:45 -0400

    Implement the ADC A, B ($88) instruction
    Title.
    
    Signed-off-by: dgz <dgz@dgz.me>

--- core/cpu.c
+++ core/cpu.c
@@ -457,6 +457,7 @@ void agoge_core_cpu_run(struct agoge_core_cpu *const cpu,
 		[CPU_OP_ADD_A_H]		= &&add_a_h,
 		[CPU_OP_ADD_A_L]		= &&add_a_l,
 		[CPU_OP_ADD_A_A]		= &&add_a_a,
+		[CPU_OP_ADC_A_B]		= &&adc_a_b,
 		[CPU_OP_XOR_A_C]		= &&xor_a_c,
 		[CPU_OP_XOR_A_L]		= &&xor_a_l,
 		[CPU_OP_XOR_A_MEM_HL]		= &&xor_a_mem_hl,
@@ -1056,6 +1057,10 @@ add_a_l:
 
 add_a_a:
 	alu_add(cpu, cpu->reg.a);
+	DISPATCH();
+
+adc_a_b:
+	alu_adc(cpu, cpu->reg.b);
 	DISPATCH();
 
 xor_a_c:
