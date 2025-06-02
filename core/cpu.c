930e58c614b3d6a60235b159fbc264c487b3326d
dgz <dgz@dgz.me>
2025-06-02 17:26:16 -0400

    Implement the SBC A, D ($9A) instruction
    Title.
    
    Signed-off-by: dgz <dgz@dgz.me>

--- core/cpu.c
+++ core/cpu.c
@@ -473,6 +473,7 @@ void agoge_core_cpu_run(struct agoge_core_cpu *const cpu,
 		[CPU_OP_SUB_A_A]		= &&sub_a_a,
 		[CPU_OP_SBC_A_B]		= &&sbc_a_b,
 		[CPU_OP_SBC_A_C]		= &&sbc_a_c,
+		[CPU_OP_SBC_A_D]		= &&sbc_a_d,
 		[CPU_OP_XOR_A_C]		= &&xor_a_c,
 		[CPU_OP_XOR_A_L]		= &&xor_a_l,
 		[CPU_OP_XOR_A_MEM_HL]		= &&xor_a_mem_hl,
@@ -1136,6 +1137,10 @@ sbc_a_b:
 
 sbc_a_c:
 	alu_sbc(cpu, cpu->reg.c);
+	DISPATCH();
+
+sbc_a_d:
+	alu_sbc(cpu, cpu->reg.d);
 	DISPATCH();
 
 xor_a_c:
