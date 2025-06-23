2e993763a16f2d31a94398423faef2c4a65ab0c4
dgz <dgz@dgz.me>
2025-06-23 17:26:02 -0400

    Implement the SWAP B ($CB $30) instruction
    Title.
    
    Signed-off-by: dgz <dgz@dgz.me>

--- core/cpu-defs.h
+++ core/cpu-defs.h
@@ -299,6 +299,7 @@
 #define CPU_OP_SRA_H	(UINT8_C(0x2C))
 #define CPU_OP_SRA_L	(UINT8_C(0x2D))
 #define CPU_OP_SRA_A	(UINT8_C(0x2F))
+#define CPU_OP_SWAP_B	(UINT8_C(0x30))
 #define CPU_OP_SWAP_A	(UINT8_C(0x37))
 #define CPU_OP_SRL_B	(UINT8_C(0x38))
 
--- core/cpu.c
+++ core/cpu.c
@@ -687,6 +687,7 @@ void agoge_core_cpu_run(struct agoge_core_cpu *const cpu,
 		[CPU_OP_SRA_H]	= &&sra_h,
 		[CPU_OP_SRA_L]	= &&sra_l,
 		[CPU_OP_SRA_A]	= &&sra_a,
+		[CPU_OP_SWAP_B]	= &&swap_b,
 		[CPU_OP_SWAP_A]	= &&swap_a,
 		[CPU_OP_SRL_B]	= &&srl_b
 
@@ -1653,6 +1654,10 @@ sra_l:
 
 sra_a:
 	cpu->reg.a = alu_sra(cpu, cpu->reg.a);
+	DISPATCH();
+
+swap_b:
+	cpu->reg.b = alu_swap(cpu, cpu->reg.b);
 	DISPATCH();
 
 swap_a:
